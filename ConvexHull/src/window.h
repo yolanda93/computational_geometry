603f2bd359875763ffe1657edb1909a7df1490aa
yolanda <yolanda93h@gmail.com>
2017-01-09 15:15:20 +0100

    added convex hull code

---
+++ ConvexHull/src/window.h
@@ -0,0 +1,48 @@
+#ifndef _WINDOW_
+#define _WINDOW_
+
+// std
+#include <list>
+
+#include <QtOpenGL/qgl.h>
+#include <CGAL/Qt/DemosMainWindow.h>
+
+#include <QWidget>
+
+// Qt
+#include <QString>
+#include "scene.h"
+#include "ui_convex.h"
+
+class MainWindow : public QMainWindow, public Ui_MainWindow
+{
+	Q_OBJECT
+
+private:
+	Scene* m_scene;
+
+	unsigned int maxNumRecentFiles;
+	QAction* recentFilesSeparator;
+	QVector<QAction*> recentFileActs;
+
+public:
+	MainWindow();
+	~MainWindow();
+
+	void update();
+
+protected:
+
+	protected slots:
+
+	// drag & drop
+	void closeEvent(QCloseEvent *event);
+
+public slots:
+
+	    // Data
+		void on_actionClear_triggered();
+		void on_actionRandom_triggered();
+};
+
+#endif // _WINDOW_
